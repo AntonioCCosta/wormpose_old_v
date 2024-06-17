f4571f7026973f5049f902a0d744919e890bff1d
Antonio Carlos Costa <antoniocbscosta@gmail.com>
2024-06-17 18:12:00 +0200

    Update postures_model.py

--- wormpose/pose/postures_model.py
+++ wormpose/pose/postures_model.py
@@ -1,6 +1,6 @@
 """
-This module exposes the gaussian mixture model of worm postures
-from the resource file "postures_model.json.gz"
+This module exposes the Gaussian mixture model of worm postures
+from the resource file "postures_model.json.gz".
 """
 
 import gzip
@@ -16,9 +16,8 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-class PosturesModel(object):
+class PosturesModel:
     def __init__(self):
-
         model_path = os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
             os.pardir,
@@ -34,18 +33,17 @@ class PosturesModel(object):
         self.weights = np.array(shapes_model["weights"])
         self.num_gaussians = self.means.shape[0]
 
-    def generate(self) -> Generator:
+    def generate(self) -> Generator[np.ndarray, None, None]:
         """
-        Generates worm postures
+        Generates worm postures.
 
-        :return: a generator of numpy arrays of shape (N,) representing the centerline angles
+        :return: A generator of numpy arrays of shape (N,) representing the centerline angles.
         """
         while True:
             idx = np.random.choice(np.arange(self.num_gaussians), p=self.weights)
             random_global_angle = np.random.uniform(0, 2 * np.pi)
 
             samples = np.random.multivariate_normal(self.means[idx], self.covariances[idx], size=1)
-
             theta = samples[0] + random_global_angle
 
             yield theta
