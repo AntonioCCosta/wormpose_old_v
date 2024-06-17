8c82090adf37dd8bc717812fcb3be8f28443b525
Antonio Carlos Costa <antoniocbscosta@gmail.com>
2024-06-17 18:10:38 +0200

    Update eigenworms.py

--- wormpose/pose/eigenworms.py
+++ wormpose/pose/eigenworms.py
@@ -1,5 +1,5 @@
 """
-Utility functions to deal with eigenworms
+Utility functions to deal with eigenworms.
 """
 
 import numpy as np
@@ -7,33 +7,31 @@ import numpy as np
 
 def load_eigenworms_matrix(eigenworms_matrix_path: str) -> np.ndarray:
     """
-    Load eigenworms matrix into numpy array from csv file
+    Load eigenworms matrix into numpy array from csv file.
 
-    :param eigenworms_matrix_path: path of the csv file
-    :return: numpy array of the eigenworms matrix
+    :param eigenworms_matrix_path: Path of the csv file.
+    :return: Numpy array of the eigenworms matrix.
     """
-    return (
-        np.loadtxt(eigenworms_matrix_path, delimiter=",").astype(float) if eigenworms_matrix_path is not None else None
-    )
+    return np.loadtxt(eigenworms_matrix_path, delimiter=",").astype(float) if eigenworms_matrix_path else None
 
 
 def theta_to_modes(theta: np.ndarray, eigenworms_matrix: np.ndarray) -> np.ndarray:
     """
     Convert angles to modes with an eigenworms matrix. We subtract the mean angle before converting.
 
-    :param theta: angle vector, numpy array of shape (N,)
-    :param eigenworms_matrix:
-    :return: the modes corresponding to the angles
+    :param theta: Angle vector, numpy array of shape (N,).
+    :param eigenworms_matrix: Eigenworms matrix.
+    :return: The modes corresponding to the angles.
     """
     return (theta - np.mean(theta)).dot(eigenworms_matrix)
 
 
 def modes_to_theta(modes: np.ndarray, eigenworms_matrix: np.ndarray) -> np.ndarray:
     """
-    Convert modes to angles with an eigenworms matrix
+    Convert modes to angles with an eigenworms matrix.
 
-    :param modes:
-    :param eigenworms_matrix:
-    :return: the angles corresponding to the modes
+    :param modes: Modes vector.
+    :param eigenworms_matrix: Eigenworms matrix.
+    :return: The angles corresponding to the modes.
     """
     return modes.dot(eigenworms_matrix[:, : len(modes)].T)
